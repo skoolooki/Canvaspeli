dce6c1032dbc4cba1ec97613a4a29e794d25e2ee
Kai Jukarainen <kai.jukarainen@gmail.com>
2022-11-22 09:40:17 +0200

    Testing

--- koodi.js
+++ koodi.js
@@ -1,2 +1,14 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
+// player class
+
+// invader class
+
+// projectile class
+
+// grid class
+
+// animate
+
+// movement
