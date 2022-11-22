318c1c124c990ee58d2233232ea28466bed1972c
Slava <shukov.sl@gmail.com>
2022-11-22 14:03:30 +0200

    Player liikkuu

--- koodi.js
+++ koodi.js
@@ -1,23 +1,111 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const enemy = {
-    x: 20,
-    y: 20,
-    width: 65,
-    height: 20,
+class Player {
+    constructor(){
+        // player Speed
+        this.speed = {
+            x: 0,
+            y: 0
+        }
+
+        const image = new Image()
+        image.src = "./images/nelio.png"
+        image.onload = ()=>{
+            const scale = 1
+            this.image = image
+            this.width = image.width * scale
+            this.height = image.height * scale
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height / 2 - this.height + 240
+            }
+        }
+    }
+
+    draw(){
+        ctx.drawImage(this.image, this.position.x, this.position.y)
+    }
+    update(){
+        if (this.image){
+            this.draw()
+            this.position.x += this.speed.x
+            // this.position.y += speed.y
+        }
+    }
 }
 
-function enemyBody(){
-    ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height)
+const player = new Player()
+
+const keys = {
+    x: {
+        pressed: false
+    },
+    y: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    }
 }
-function enemies(){
-    enemyBody()
 
-    requestAnimationFrame(enemies)
-    console.log(123)
+function playerMovement(){
+    if (keys.x.pressed && player.position.x >= 0){
+        player.speed.x = -5
+    } else if (keys.y.pressed && player.position.x + player.width <= canvas.width ){
+        player.speed.x = 5
+    } else {
+        player.speed.x = 0
+    }
 }
 
+function animate(){
+    requestAnimationFrame(animate)
+    ctx.fillRect(0,0,canvas.width,canvas.height)
+    player.update()
+    playerMovement()
+}
 
-enemies()
-player()
+animate()
+
+
+addEventListener("keydown",({key})=>{
+    switch(key){
+        case "x": keys.x.pressed = true
+        break
+        case "y": keys.y.pressed = true
+        break
+        case " ": keys.space.pressed = true
+        break
+    }
+})
+addEventListener("keyup",({key})=>{
+    switch(key){
+        case "x": keys.x.pressed = false
+        break
+        case "y": keys.y.pressed = false
+        break
+        case " ": keys.space.pressed = false
+        break
+    }
+})
+
+// const player = {
+//     x: 20,
+//     y: 20,
+//     width: 65,
+//     height: 20,
+// }
+
+// function playerBody(){
+//     ctx.fillRect(player.x, player.y, player.width, player.height)
+// }
+// function enemies(){
+//     playerBody()
+
+//     requestAnimationFrame(enemies)
+//     console.log(123)
+// }
+
+
+// enemies()
