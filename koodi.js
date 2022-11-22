78927b03856eec7560e2e8a73a5073ec0adfdb5a
Slava <shukov.sl@gmail.com>
2022-11-22 11:16:52 +0200

    a

--- koodi.js
+++ koodi.js
@@ -1,2 +1,23 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
+const enemy = {
+    x: 20,
+    y: 20,
+    width: 65,
+    height: 20,
+}
+
+function enemyBody(){
+    ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height)
+}
+function enemies(){
+    enemyBody()
+
+    requestAnimationFrame(enemies)
+    console.log(123)
+}
+
+
+enemies()
+player()
